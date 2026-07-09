cb032684c0eebadf696d2ec9adfd53ed11078f48
Mayuresh <mayuresh@warunjikar.in>
2026-07-09 17:56:26 +0530

    WIP: yaml IR

--- yaml2h.cpp
+++ yaml2h.cpp
@@ -15,9 +15,12 @@ using namespace std;
 
 constexpr string_view kwd_constants  = "constants";
 constexpr string_view kwd_types      = "types";
-constexpr string_view kwd_domains    = "domain";
-constexpr string_view kwd_unions     = "union";
-constexpr string_view kwd_structures = "structure";
+constexpr string_view kwd_domain     = "domain";
+constexpr string_view kwd_union      = "union";
+constexpr string_view kwd_structure  = "structure";
+constexpr string_view kwd_kind       = "kind";
+constexpr string_view kwd_descr      = "descr";
+constexpr string_view kwd_values     = "values";
 
 class Type
 {
@@ -42,6 +45,7 @@ class Structure : public Type
 class YamlSpec
 {
     map<string,string> _constants;
+    map<string,Type*> _types;
     void check_file_exists(char *path)
     {
         if ( not filesystem::exists(path) )
@@ -61,31 +65,78 @@ class YamlSpec
         }
         return "unknown";
     }
+    void print_err_loc(const YAML::Node& node, char* path)
+    {
+        cerr << " " << path << ":";
+        auto mark = node.Mark();
+        if ( mark.is_null() ) cerr << "unknown";
+        else cerr << mark.line + 1 << ":" << mark.column + 1;
+        cerr << endl;
+    }
     template <YAML::NodeType::value Expected>
     void expectType(const YAML::Node& node,char* path)
     {
         if (node.Type() != Expected)
         {
-            auto mark = node.Mark();
-            cerr << "Expected yaml object of type: " << nodeTypeName(Expected)
-                << " : " << path << ":";
-            if ( mark.is_null() ) cerr << "unknown";
-            else cerr << mark.line + 1 << ":" << mark.column + 1;
-            cerr << endl;
+            cerr << "Expected yaml object of type: " << nodeTypeName(Expected);
+            print_err_loc(node,path);
             exit(1);
         }
     }
-    void process_domain(YAML::Node& node, char* path)
+    void expectKey(const YAML::Node& node,string_view key,char* path)
     {
+        // Assumption: Caller does expectType
+        if ( not node[key] )
+        {
+            auto mark = node.Mark();
+            cerr << "Key '" << key << "' missing in map";
+            print_err_loc(node,path);
+            exit(1);
+        }
     }
-    void process_union(YAML::Node& node, char* path)
+    void process_domain(string name, YAML::Node& node, char* path)
     {
+        expectKey(node,kwd_values,path);
+        _types.emplace(name,nullptr);
     }
-    void process_structure(YAML::Node& node, char* path)
+    void process_union(string name, YAML::Node& node, char* path)
     {
+        _types.emplace(name,nullptr);
+    }
+    void process_structure(string name, YAML::Node& node, char* path)
+    {
+        _types.emplace(name,nullptr);
+    }
+    void process_type(string name, YAML::Node& node, char* path)
+    {
+        expectType<YAML::NodeType::Map> (node,path);
+        expectKey(node,kwd_kind,path);
+        expectKey(node,kwd_descr,path);
+        auto kind = node[kwd_kind].as<string>();
+        if ( kind == kwd_domain ) process_domain(name,node,path);
+        else if ( kind == kwd_union ) process_union(name,node,path);
+        else if ( kind == kwd_structure ) process_structure(name,node,path);
+        else
+        {
+            cerr << "Unknown kind: " << kind << " in "
+                << path << ":" << name << endl;
+            exit(1);
+        }
     }
     void process_types(YAML::Node& node, char* path)
     {
+        expectType<YAML::NodeType::Map> (node,path);
+        for (const auto& kv : node)
+        {
+            auto key = kv.first.as<string>();
+            if ( _types.find(key) != _types.end() )
+            {
+                cerr << "Duplicate type found " << path << ":" << key << endl;
+                exit(1);
+            }
+            auto value = kv.second;
+            process_type(key,value,path);
+        }
     }
     void process_constant(string key, string value, char* path)
     {
@@ -95,6 +146,7 @@ class YamlSpec
         {
             cerr << "Duplicate constant found "
                 << path << " : " << key << endl;
+            exit(1);
         }
     }
     void process_constants(YAML::Node& node, char* path)
